6b5c35ceeac43c2be95bfaa3edc8323a5a0feab1
calebgregory <calebgregory@gmail.com>
2015-09-17 10:49:33 -0500

    Adding external-webpage form and route

--- node_server/app/home/controller.js
+++ node_server/app/home/controller.js
@@ -4,3 +4,7 @@ export function index(req,res) {
   res.render('home/index',
              { page : 'Home' });
 };
+
+export function externalWebpage(req,res) {
+  res.send(req.query);
+}
--- node_server/app/home/routes.js
+++ node_server/app/home/routes.js
@@ -6,5 +6,6 @@ var express = require('express')
 var ctrl = require('./controller');
 
 router.get('/', ctrl.index);
+router.get('/external-webpage', ctrl.externalWebpage);
 
 module.exports = router;
