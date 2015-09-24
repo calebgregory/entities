e9375fd5b36c169a9bdc93edf8b3bff34408843c
calebgregory <calebgregory@gmail.com>
2015-09-23 22:57:22 -0500

    Removing nltk and adding visiter

--- python_server/framework/tasks.py
+++ python_server/framework/tasks.py
@@ -1,9 +1,8 @@
 from __future__ import absolute_import
 
 from framework.celery import app
-import nltk
+from visiter import visit
 
 @app.task
-def tokenize(message):
-    tokens = nltk.word_tokenize(message)
-    return tokens
+def visitPage(url):
+    return visit(url)
