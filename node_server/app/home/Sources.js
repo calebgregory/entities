463c81ff651af02e06554ca6d7ad24da91dfd2dc
calebgregory <calebgregory@gmail.com>
2015-09-25 00:57:15 -0500

    Adding short explanation of data

---
+++ node_server/app/home/Sources.js
@@ -0,0 +1,36 @@
+'use strict;'
+
+var pg = require('pg');
+var conString = process.env.SENTIMENT_DB_URL || 'postgres://localhost/testdb';
+
+export function getValuatedSources(cb) {
+
+  pg.connect(conString, (err,client,done) => {
+
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+
+    client.query(
+      'SELECT  S.sourceid, S.name, SUM(sv.value) '+
+      'FROM sources AS S '+
+      'INNER JOIN visitedLinks AS VL '+
+      'ON VL.sourceid = S.sourceid '+
+      'INNER JOIN associations AS A '+
+      'ON A.linkid = VL.linkid '+
+      'INNER JOIN sentimentval AS SV '+
+      'ON SV.word = A.word '+
+      'GROUP BY S.sourceid, S.name '+
+      'ORDER BY S.sourceid;',
+      [],
+      (err,result) => {
+        if(err) return console.error('error running query', err);
+
+        cb(null, result.rows);
+        client.end();
+
+      });
+
+  });
+
+}
