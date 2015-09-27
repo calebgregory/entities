8bcc3d4d426d79e37e1d8297d214f91295f22bb3
calebgregory <calebgregory@gmail.com>
2015-09-26 23:11:32 -0500

    Removing other directories

--- node_server/lib/errorHandler/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var app                        = require('../../app/')
-  , handleInternalServerErrors = require('./lib/internalServer')
-  , handleNotFoundErrors       = require('./lib/notFound');
-
-app.use(handleNotFoundErrors());
-app.use(handleInternalServerErrors());
--- python_server/framework/tasks.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from framework.celery import app
-import nltk
-
-@app.task
-def tokenize(message):
-    tokens = nltk.word_tokenize(message)
-    return tokens
