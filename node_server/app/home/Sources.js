cfb275add8fe83014e6e8386dd28f5f16c24b7fc
calebgregory <calebgregory@gmail.com>
2015-09-26 22:38:37 -0500

    Adding API average instead of grabbing this from SENTIMENT_DB

--- node_server/app/home/Sources.js
+++ node_server/app/home/Sources.js
@@ -1,7 +1,7 @@
 'use strict;'
 
 var pg = require('pg');
-var conString = process.env.SENTIMENT_DB_URL || 'postgres://localhost/testdb';
+var conString = process.env.API_DB_URL || 'postgres://localhost/testdb2';
 
 export function getValuatedSources(cb) {
 
@@ -12,16 +12,7 @@ export function getValuatedSources(cb) {
     }
 
     client.query(
-      'SELECT  S.sourceid, S.name, SUM(sv.value), COUNT(vl.linkid) '+
-      'FROM sources AS S '+
-      'INNER JOIN visitedLinks AS VL '+
-      'ON VL.sourceid = S.sourceid '+
-      'INNER JOIN associations AS A '+
-      'ON A.linkid = VL.linkid '+
-      'INNER JOIN sentimentval AS SV '+
-      'ON SV.word = A.word '+
-      'GROUP BY S.sourceid, S.name '+
-      'ORDER BY SUM(SV.value) DESC;',
+      'SELECT * FROM sentimentbysource ORDER BY created ASC LIMIT 6;',
       [],
       (err,result) => {
         if(err) return console.error('error running query', err);
